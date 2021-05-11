2a7ba306a94e229b4cab33b10fab90924445bad9
aharri64 <haveacompleteday@gmail.com>
2021-05-10 20:52:46 -0600

    navbar detail

--- client-app/src/app/layouts/NavBar.tsx
+++ client-app/src/app/layouts/NavBar.tsx
@@ -6,7 +6,7 @@ export default function NavBar() {
         <Menu inverted fixed='top'>
             <Container>
                 <Menu.Item header>
-                    <img src='/assets/logo.png' alt='Logo' />
+                    <img src='/assets/logo.png' alt='Logo' style={{ marginRight: '10px' }} />
                     HobNob
                 </Menu.Item>
                 <Menu.Item name='Activities' />
