a504c41ef87d6e6b4ddd0a3c1af6cb61615c3e9f
aharri64 <haveacompleteday@gmail.com>
2021-05-13 10:43:11 -0600

    58. delete and activity

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -5,9 +5,10 @@ import { Activity } from '../../../app/models/activity'
 interface Props {
     activities: Activity[];
     selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
 }
 
-export default function ActivityList({ activities, selectActivity }: Props) {
+export default function ActivityList({ activities, selectActivity, deleteActivity }: Props) {
     return (
         <Segment>
             <Item.Group divided>
@@ -29,7 +30,8 @@ export default function ActivityList({ activities, selectActivity }: Props) {
                                 </div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button onClick={() => selectActivity(activity.id)} floated="right" content="view" color="blue" />
+                                <Button onClick={() => selectActivity(activity.id)} floated="right" content="View" color="blue" />
+                                <Button onClick={() => deleteActivity(activity.id)} floated="right" content="Delete" color="red" />
                                 <Label basic content={activity.category} />
                             </Item.Extra>
                         </Item.Content>
