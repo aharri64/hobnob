6062e5d66b725e2823da010c53d1235162b889f3
aharri64 <haveacompleteday@gmail.com>
2021-05-31 20:58:04 -0600

    64. posting data to the server

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
+import { request } from 'http';
 import { Activity } from '../models/activity';
 
 const sleep = (delay: number) => {
@@ -29,7 +30,11 @@ const requests = {
 }
 
 const Activities = {
-    list: () => requests.get<Activity[]>('/activities')
+    list: () => requests.get<Activity[]>('/activities'),
+    details: () => (id: string) => requests.get<Activity>(`/activities/${id}`),
+    create: (activity: Activity) => requests.post<void>('/activities', activity),
+    update: (activity: Activity) => requests.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.del<void>(`/activities/${id}`)
 }
 
 const agent = {
