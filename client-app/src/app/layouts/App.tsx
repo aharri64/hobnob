c0a7020392b51fe0ebb98eecf83717cbcdf5802a
aharri64 <haveacompleteday@gmail.com>
2021-06-01 22:18:15 -0600

    70. ModX Actions

--- client-app/src/app/layouts/App.tsx
+++ client-app/src/app/layouts/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from 'semantic-ui-react';
+import { Button, Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
@@ -7,6 +7,7 @@ import { v4 as uuid } from 'uuid';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 
 function App() {
@@ -82,7 +83,7 @@ function App() {
       <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: '7em' }}>
         <h2>{activityStore.title}</h2>
-
+        <Button content='Add exclamation!' positive onClick={activityStore.setTitle} />
         <ActivityDashboard
           activities={activities}
           selectedActivity={selectedActivity}
@@ -100,4 +101,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
