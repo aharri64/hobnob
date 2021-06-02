ace9440e34c334bccdab30f05579621ad054d899
aharri64 <haveacompleteday@gmail.com>
2021-06-01 22:04:10 -0600

    69. setting up mobx

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,10 +4,12 @@ import 'semantic-ui-css/semantic.min.css';
 import './app/layouts/styles.css';
 import App from './app/layouts/App';
 import reportWebVitals from './reportWebVitals';
+import { store, StoreContext } from './app/stores/store';
 
 ReactDOM.render(
-
-  <App />,
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>,
   document.getElementById('root')
 );
 
