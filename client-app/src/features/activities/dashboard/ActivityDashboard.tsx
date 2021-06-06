e7985f09ad622e60de85347bcb0ee0050d9ba516
aharri64 <haveacompleteday@gmail.com>
2021-06-05 22:29:47 -0600

    74. deleting an activity using mobx

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,37 +1,23 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
 
-interface Props {
-    activities: Activity[];
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
-
-export default observer(function ActivityDashboard({ activities, deleteActivity, submitting }: Props) {
+export default observer(function ActivityDashboard() {
 
     const { activityStore } = useStore();
     const { selectedActivity, editMode } = activityStore;
     return (
         <Grid>
             <Grid.Column width="10">
-                <ActivityList
-                    activities={activities}
-                    deleteActivity={deleteActivity}
-                    submitting={submitting}
-                />
+                <ActivityList />
             </Grid.Column>
             <Grid.Column width="6">
-                {selectedActivity && !editMode &&
-                    <ActivityDetails />
-                }
-                {editMode &&
-                    <ActivityForm />}
+                {selectedActivity && !editMode && <ActivityDetails />}
+                {editMode && <ActivityForm />}
             </Grid.Column>
         </Grid>
     )
