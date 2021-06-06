e1c2f3e5621fc2fb62f1132492ff5b10a8978fd8
aharri64 <haveacompleteday@gmail.com>
2021-06-05 22:02:37 -0600

    74. creating an activity using mobx

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -9,12 +9,11 @@ import ActivityList from './ActivityList';
 
 interface Props {
     activities: Activity[];
-    createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
     submitting: boolean;
 }
 
-export default observer(function ActivityDashboard({ activities, createOrEdit, deleteActivity, submitting }: Props) {
+export default observer(function ActivityDashboard({ activities, deleteActivity, submitting }: Props) {
 
     const { activityStore } = useStore();
     const { selectedActivity, editMode } = activityStore;
@@ -32,10 +31,7 @@ export default observer(function ActivityDashboard({ activities, createOrEdit, d
                     <ActivityDetails />
                 }
                 {editMode &&
-                    <ActivityForm
-                        createOrEdit={createOrEdit}
-                        submitting={submitting}
-                    />}
+                    <ActivityForm />}
             </Grid.Column>
         </Grid>
     )
