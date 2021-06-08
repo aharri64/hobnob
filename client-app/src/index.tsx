6448826ffcbf84bb434b2239f5170fa36cf7def6
aharri64 <haveacompleteday@gmail.com>
2021-06-08 00:01:59 -0600

    add browserrouter

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,10 +5,14 @@ import './app/layouts/styles.css';
 import App from './app/layouts/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
+import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StoreContext.Provider>,
   document.getElementById('root')
 );
