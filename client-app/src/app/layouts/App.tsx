0b9aa1d128204c5462f193bd175d33eff2feda58
aharri64 <haveacompleteday@gmail.com>
2021-06-10 00:04:23 -0600

    add routes

--- client-app/src/app/layouts/App.tsx
+++ client-app/src/app/layouts/App.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
 import { Route } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
@@ -11,19 +9,11 @@ import ActivityForm from '../../features/activities/form/ActivityForm';
 
 
 function App() {
-  const { activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore])
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
-
   return (
     <>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <Route path='/' component={HomePage} />
+        <Route exact path='/' component={HomePage} />
         <Route path='/activities' component={ActivityDashboard} />
         <Route path='/createActivity' component={ActivityForm} />
       </Container>
