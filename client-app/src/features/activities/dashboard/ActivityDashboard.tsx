22acc5c30b22de2de2d297dbdffeb66d32ca36c3
aharri64 <haveacompleteday@gmail.com>
2021-06-10 00:04:29 -0600

    add routes

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
+import LoadingComponent from '../../../app/layouts/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
@@ -10,6 +11,13 @@ export default observer(function ActivityDashboard() {
 
     const { activityStore } = useStore();
     const { selectedActivity, editMode } = activityStore;
+
+    useEffect(() => {
+        activityStore.loadActivities();
+    }, [activityStore])
+
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+
     return (
         <Grid>
             <Grid.Column width="10">
