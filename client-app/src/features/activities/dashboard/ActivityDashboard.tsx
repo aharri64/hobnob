5f93faa54ad9b152f91ead1bb6e886d44762c04c
aharri64 <haveacompleteday@gmail.com>
2021-06-17 19:16:34 -0600

    Cleaning up unused code

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -8,10 +8,11 @@ import ActivityList from './ActivityList';
 export default observer(function ActivityDashboard() {
 
     const { activityStore } = useStore();
+    const { loadActivities, activityRegistry } = activityStore;
 
     useEffect(() => {
-        activityStore.loadActivities();
-    }, [activityStore])
+        if (activityRegistry.size <= 1) loadActivities();
+    }, [activityRegistry.size, loadActivities])
 
     if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
 
