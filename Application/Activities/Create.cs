e73dff31343b119ee769f46433388591b6edcaa7
aharri64 <haveacompleteday@gmail.com>
2021-07-06 16:54:38 -0600

    fluent validation

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,6 +1,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -12,6 +13,15 @@ namespace Application.Activities
         {
             public Activity Activity { get; set; }
         }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+            }
+        }
+
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
