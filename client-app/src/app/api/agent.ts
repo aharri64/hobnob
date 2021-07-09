faf9cc18600de5036bf7e41a776c24d07f7088c1
aharri64 <haveacompleteday@gmail.com>
2021-07-09 16:52:46 -0600

    Handling 400 errors

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -21,7 +21,17 @@ axios.interceptors.response.use(async response => {
     const { data, status } = error.response!;
     switch (status) {
         case 400:
-            toast.error('bad request');
+            if (data.errors) {
+                const modalStateErrors = [];
+                for (const key in data.errors) {
+                    if (data.errors[key]) {
+                        modalStateErrors.push(data.errors[key])
+                    }
+                }
+                throw modalStateErrors.flat();
+            } else {
+                toast.error(data);
+            }
             break;
         case 401:
             toast.error('unauthorized');
