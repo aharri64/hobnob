5926e8087b3f85fedd2975df47db2ee740665ed4
aharri64 <haveacompleteday@gmail.com>
2021-07-15 11:15:54 -0600

    creating a reusable date input

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -11,6 +11,7 @@ import MyTextInput from '../../../app/common/form/MyTextInput'
 import MyTextArea from '../../../app/common/form/MyTextArea'
 import MySelectInput from '../../../app/common/form/MySelectInput'
 import { categoryOptions } from '../../../app/common/options/categoryOptions'
+import MyDateInput from '../../../app/common/form/MyDateInput'
 
 export default observer(function ActivityForm() {
     const history = useHistory()
@@ -72,7 +73,13 @@ export default observer(function ActivityForm() {
                         <MyTextInput name='title' placeholder='Title' />
                         <MyTextArea rows={3} placeholder='Description' name='description' />
                         <MySelectInput options={categoryOptions} placeholder='Category' name='category' />
-                        <MyTextInput placeholder='Date' name='date' />
+                        <MyDateInput
+                            placeholderText='Date'
+                            name='date'
+                            showTimeSelect
+                            timeCaption='time'
+                            dateFormat='MMMM d, yyyy h:m aa'
+                        />
                         <MyTextInput placeholder='City' name='city' />
                         <MyTextInput placeholder='Venue' name='venue' />
                         <Button loading={loading} floated='right' positive type='submit' content='Submit' />
