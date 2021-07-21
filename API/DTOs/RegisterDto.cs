15b5e7d11f0bc67266d187f24290bceb2e090751
aharri64 <haveacompleteday@gmail.com>
2021-07-20 23:50:00 -0600

    Validating the registration of users

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password Must contain one lower case letter, one upper case letter, one number, and be between 4 and 8 characters.")]
         public string Password { get; set; }
+
+        [Required]
         public string Username { get; set; }
     }
 }
