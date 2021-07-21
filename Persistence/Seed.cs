c342fef2240c3fd8539390dded6353892d5758a8
aharri64 <haveacompleteday@gmail.com>
2021-07-20 21:22:46 -0600

    adding seed users

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context)//Passing in data
+        public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)//Passing in data
         {
+            // check to see if there are any users already in the DB
+            if (!userManager.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    new AppUser{DisplayName= "Bob", UserName = "bob", Email = "bob@test.com" },
+                    new AppUser{DisplayName= "Tom", UserName = "tob", Email = "tom@test.com"},
+                    new AppUser{DisplayName= "Jane", UserName = "jane", Email = "jane@test.com"},
+                };
+
+
+                foreach (var user in users)
+                {
+                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
+
             if (context.Activities.Any()) return; //? check if there are any activities and return
 
             var activities = new List<Activity>
