db64cffe504bf61672119134079f6f57c940303c
aharri64 <haveacompleteday@gmail.com>
2021-07-23 15:56:44 -0600

    Creating a login form

--- client-app/src/app/layouts/App.tsx
+++ client-app/src/app/layouts/App.tsx
@@ -11,6 +11,7 @@ import TestErrors from '../../features/errors/TestError';
 import { ToastContainer } from 'react-toastify';
 import NotFound from '../../features/errors/NotFound';
 import ServerError from '../../features/errors/ServerError';
+import LoginForm from '../../features/users/LoginForm';
 
 
 function App() {
@@ -35,6 +36,7 @@ function App() {
                 />
                 <Route path='/errors' component={TestErrors} />
                 <Route path='/server-error' component={ServerError} />
+                <Route path='/login' component={LoginForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -11,8 +11,8 @@ export default function HomePage() {
                     Hobnob
                 </Header>
                 <Header as='h2' inverted content='Welcome to Hobnob' />
-                <Button as={Link} to='/activities' size='huge' inverted >
-                    Take me to the Activities!
+                <Button as={Link} to='/login' size='huge' inverted >
+                    Login!
                 </Button>
             </Container>
         </Segment>
