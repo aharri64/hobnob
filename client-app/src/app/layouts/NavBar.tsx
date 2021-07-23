c9e6376a90239e353c33d100ff5470009b7a95fa
aharri64 <haveacompleteday@gmail.com>
2021-07-23 17:12:35 -0600

    updating the homepage and navbar

--- client-app/src/app/layouts/NavBar.tsx
+++ client-app/src/app/layouts/NavBar.tsx
@@ -1,11 +1,12 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Button, Container, Menu } from 'semantic-ui-react';
-// import { useStore } from '../stores/store';
+import { Link, NavLink } from 'react-router-dom';
+import { Button, Container, Dropdown, Image, Menu } from 'semantic-ui-react';
+import { useStore } from '../stores/store';
 
-export default function NavBar() {
+export default observer(function NavBar() {
 
-    // const { activityStore } = useStore();
+    const { userStore: { user, logout } } = useStore();
 
     return (
         <Menu inverted fixed='top'>
@@ -19,8 +20,18 @@ export default function NavBar() {
                 <Menu.Item >
                     <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
                 </Menu.Item>
+                <Menu.Item position='right'>
+                    <Image src={user?.image || '/assets/user.png'} avatar spaced='right' />
+                    <Dropdown pointing='top left' text={user?.displayName} >
+                        <Dropdown.Menu>
+                            <Dropdown.Item as={Link} to={`/profile/${user?.username}`}
+                                text='My Profile' icon='user' />
+                            <Dropdown.Item onClick={logout} text='logout' icon='power' />
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Item>
             </Container>
 
         </Menu>
     )
-}
+})
