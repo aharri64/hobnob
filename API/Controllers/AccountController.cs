f337fed4f959fd317e7c9ec26a78ce16d5030ee3
aharri64 <haveacompleteday@gmail.com>
2021-07-23 19:14:46 -0600

    handling validation errors in the register form

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -47,11 +47,13 @@ namespace API.Controllers
         {
             if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
             {
-                return BadRequest("Email Taken");
+                ModelState.AddModelError("email", "Email Taken");
+                return ValidationProblem();
             }
             if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
             {
-                return BadRequest("Username Taken");
+                ModelState.AddModelError("username", "Username taken");
+                return ValidationProblem();
             }
 
             var user = new AppUser
--- client-app/src/features/users/RegisterForm.tsx
+++ client-app/src/features/users/RegisterForm.tsx
@@ -1,10 +1,11 @@
 import { ErrorMessage, Form, Formik } from 'formik'
 import { observer } from 'mobx-react-lite';
 import React from 'react'
-import { Button, Header, Label } from 'semantic-ui-react'
+import { Button, Header } from 'semantic-ui-react'
 import MyTextInput from '../../app/common/form/MyTextInput'
 import { useStore } from '../../app/stores/store';
 import * as Yup from 'yup';
+import ValidationErrors from '../errors/ValidationErrors';
 
 export default observer(function RegisterForm() {
     const { userStore } = useStore();
@@ -12,7 +13,7 @@ export default observer(function RegisterForm() {
         <Formik
             initialValues={{ displayName: '', username: '', email: '', password: '', error: null }}
             onSubmit={(values, { setErrors }) => userStore.register(values).catch(error =>
-                setErrors({ error: 'Invalid email or password' }))}
+                setErrors({ error }))}
             validationSchema={Yup.object({
                 displayName: Yup.string().required(),
                 username: Yup.string().required(),
@@ -21,7 +22,7 @@ export default observer(function RegisterForm() {
             })}
         >
             {({ handleSubmit, isSubmitting, errors, isValid, dirty }) => (
-                <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+                <Form className='ui form error' onSubmit={handleSubmit} autoComplete='off'>
                     <Header as='h2' content='Sign up to Hobnob' color='teal' textAlign='center' />
                     <MyTextInput name='displayName' placeholder='Display Name' />
                     <MyTextInput name='username' placeholder='Username' />
@@ -29,7 +30,7 @@ export default observer(function RegisterForm() {
                     <MyTextInput name='password' placeholder='Password' type='password' />
                     <ErrorMessage
                         name='error' render={() =>
-                            <Label style={{ marginBottom: 10 }} basic color='red' content={errors.error} />}
+                            <ValidationErrors errors={errors.error} />}
                     />
                     <Button disabled={!isValid || !dirty || isSubmitting}
                         loading={isSubmitting} positive content='Register' type='submit' fluid />
