cf248a1e6fb90969636794a7af200a2bea4db297
aharri64 <haveacompleteday@gmail.com>
2021-07-24 21:38:04 -0600

    shaping the related data

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,18 +1,19 @@
 using System;
 using System.Collections.Generic;
+using Application.Profiles;
 
-namespace Domain
+namespace Application.Activities
 {
-    public class Activity
+    public class ActivityDto
     {
-        // When we return an activity, we will return an object with these properties
-        public Guid Id { get; set; } //? we can do this all inside the client side (primary key)
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
-        public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
+        public string HostUsername { get; set; }
+        public ICollection<Profile> Profiles { get; set; }
     }
 }
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -11,25 +12,28 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Activity>>> { }
+        public class Query : IRequest<Result<List<ActivityDto>>> { }
 
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
 
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activities = await _context.Activities
                     .Include(a => a.Attendees)
                     .ThenInclude(u => u.AppUser)
                     .ToListAsync(cancellationToken);
 
+                var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
 
-                return Result<List<Activity>>.Success(activities);
+                return Result<List<ActivityDto>>.Success(activitiesToReturn);
             }
         }
     }
