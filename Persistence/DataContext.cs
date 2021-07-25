ce82a1c47ef08226c025010a966a5c11f381223a
aharri64 <haveacompleteday@gmail.com>
2021-07-24 20:20:12 -0600

    configuring the new relationship

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -10,5 +10,23 @@ namespace Persistence
         {
         }
         public DbSet<Activity> Activities { get; set; } //? This will represent a table called activities
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
+
+            builder.Entity<ActivityAttendee>()
+                .HasOne(u => u.AppUser)
+                .WithMany(a => a.Activities)
+                .HasForeignKey(aa => aa.AppUserId);
+
+            builder.Entity<ActivityAttendee>()
+                .HasOne(u => u.Activity)
+                .WithMany(a => a.Attendees)
+                .HasForeignKey(aa => aa.ActivityId);
+        }
     }
 }
