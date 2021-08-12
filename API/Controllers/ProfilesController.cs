6df52c01f2a59876d50768853c1a11ea92b7a822
aharri64 <haveacompleteday@gmail.com>
2021-08-11 18:59:49 -0600

    updating the mapping config

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -9,7 +9,7 @@ namespace API.Controllers
         [HttpGet("{username}")]
         public async Task<IActionResult> GetProfile(string username)
         {
-            return HandleResult(await Mediator.Send(new Details.Query { Username = username }))
+            return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
         }
     }
 }
