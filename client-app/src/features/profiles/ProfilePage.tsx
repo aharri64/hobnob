b7389697a02238a1b787e898a4e4acbf1e80656c
aharri64 <haveacompleteday@gmail.com>
2021-08-16 15:18:40 -0600

    Displaying the photos

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -23,8 +23,11 @@ export default observer(function ProfilePage() {
             <Grid>
                 <Grid.Column width={16}>
                     {profile &&
-                        <ProfileHeader profile={profile} />}
-                    <ProfileContent />
+                        <>
+                            <ProfileHeader profile={profile} />
+                            <ProfileContent profile={profile} />
+                        </>
+                    }
                 </Grid.Column>
             </Grid>
         </h1>
