7785627108893b47900d9f1830ecd9da7be092ef
aharri64 <haveacompleteday@gmail.com>
2021-08-16 13:47:21 -0600

    adding a profile header

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Grid } from 'semantic-ui-react'
+import ProfileHeader from './ProfileHeader'
 
 export default function ProfilePage() {
     return (
         <h1>
-            Profile
+            <Grid>
+                <Grid.Column width={16}>
+                    <ProfileHeader />
+                </Grid.Column>
+            </Grid>
         </h1>
     )
 }
