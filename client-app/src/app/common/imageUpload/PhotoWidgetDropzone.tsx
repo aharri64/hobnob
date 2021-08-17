da4b162533a010a037ee296e110297ad2291ca80
aharri64 <haveacompleteday@gmail.com>
2021-08-17 14:06:49 -0600

    styling the dropzone

--- client-app/src/app/common/imageUpload/PhotoWidgetDropzone.tsx
+++ client-app/src/app/common/imageUpload/PhotoWidgetDropzone.tsx
@@ -1,20 +1,37 @@
 import React, { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
+import { Header, Icon } from 'semantic-ui-react'
 
-export default function PhotoWidgetDropzone() {
+interface Props {
+    setFiles: (files: any) => void;
+}
+
+export default function PhotoWidgetDropzone({ setFiles }: Props) {
+    const dzStyles = {
+        border: 'dashed 3px #eee',
+        borderColor: '#eee',
+        borderRadius: '5px',
+        paddingTop: '30px',
+        textAlign: 'center' as 'center',
+        height: 200
+    }
+
+    const dzActive = {
+        borderColor: 'green',
+    }
+
     const onDrop = useCallback(acceptedFiles => {
-        console.log(acceptedFiles);
-    }, [])
+        setFiles(acceptedFiles.map((file: any) => Object.assign(file, {
+            preview: URL.createObjectURL(file)
+        })))
+    }, [setFiles])
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
     return (
-        <div {...getRootProps()}>
+        <div {...getRootProps()} style={isDragActive ? { ...dzStyles, ...dzActive } : dzStyles}>
             <input {...getInputProps()} />
-            {
-                isDragActive ?
-                    <p>Drop the files here ...</p> :
-                    <p>Drag 'n' drop some files here, or click to select files</p>
-            }
+            <Icon name='upload' size='huge' />
+            <Header content='Drop image here' />
         </div>
     )
 }
