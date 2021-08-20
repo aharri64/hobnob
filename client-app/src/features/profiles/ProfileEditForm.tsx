9f6f403a430b31a781770e14daef86a2ccb37e76
aharri64 <haveacompleteday@gmail.com>
2021-08-19 22:36:00 -0600

    Create a Profile Edit form component

---
+++ client-app/src/features/profiles/ProfileEditForm.tsx
@@ -0,0 +1,48 @@
+import { Formik } from 'formik';
+import { observer } from 'mobx-react-lite'
+import React from 'react'
+import { Button, Form } from 'semantic-ui-react';
+import MyTextArea from '../../app/common/form/MyTextArea';
+import MyTextInput from '../../app/common/form/MyTextInput';
+import { useStore } from '../../app/stores/store';
+import * as Yup from 'yup';
+
+interface Props {
+    setEditMode: (editMode: boolean) => void;
+}
+
+export default observer(function ProfileEditForm({ setEditMode }: Props) {
+    const { profileStore: { profile, updateProfile } } = useStore();
+
+    return (
+        <Formik
+            initialValues={{ displayName: profile?.displayName, bio: profile?.bio }}
+            onSubmit={values => {
+                updateProfile(values).then(() => {
+                    setEditMode(false);
+                })
+            }}
+            validationSchema={Yup.object({
+                displayName: Yup.string().required()
+            })}
+        >
+            {({ isSubmitting, isValid, dirty }) => (
+                <Form className='ui form'>
+                    <MyTextInput placeholder='Display Name' name='displayName' />
+                    <MyTextArea rows={3} placeholder='Tell people about yourself' name='bio' />
+                    <Button
+                        positive
+                        type='submit'
+                        loading={isSubmitting}
+                        content='Update Profile'
+                        floated='right'
+                        disabled={!isValid || !dirty}
+                    />
+                </Form>
+            )}
+        </Formik>
+    )
+})
+
+
+
