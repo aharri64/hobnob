f65dda887a48a730200f394718126386ce18a523
aharri64 <haveacompleteday@gmail.com>
2021-08-23 19:58:23 -0600

    setting up the comment entity

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,6 +12,7 @@ namespace Persistence
         public DbSet<Activity> Activities { get; set; } //? This will represent a table called activities
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Comment> Comments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -28,6 +29,11 @@ namespace Persistence
                 .HasOne(u => u.Activity)
                 .WithMany(a => a.Attendees)
                 .HasForeignKey(aa => aa.ActivityId);
+
+            builder.Entity<Comment>()
+                .HasOne(a => a.Activity)
+                .WithMany(c => c.Comments)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
