e2d8f8ab039e3e1f82eead3ba1664d2ce459c75d
aharri64 <haveacompleteday@gmail.com>
2021-08-25 11:17:52 -0600

    connecting to the hub

--- API/Extentions/ApplicationServiceExtensions.cs
+++ API/Extentions/ApplicationServiceExtensions.cs
@@ -29,7 +29,11 @@ namespace API.Extentions
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                    policy
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                    .WithOrigins("http://localhost:3000");
                 });
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
