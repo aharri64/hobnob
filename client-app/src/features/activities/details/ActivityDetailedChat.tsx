c845c4e349e9ea3b4c18ec90d0e61f67a6c23276
aharri64 <haveacompleteday@gmail.com>
2021-08-26 20:18:07 -0600

    adding validation and fix issues

--- client-app/src/features/activities/details/ActivityDetailedChat.tsx
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -1,10 +1,11 @@
-import { Formik, Form } from 'formik';
+import { Formik, Form, Field, FieldProps } from 'formik';
 import { observer } from 'mobx-react-lite'
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom';
-import { Segment, Header, Comment, Button } from 'semantic-ui-react'
+import { Segment, Header, Comment, Button, Loader } from 'semantic-ui-react'
 import MyTextArea from '../../../app/common/form/MyTextArea';
 import { useStore } from '../../../app/stores/store';
+import * as Yup from 'yup';
 
 interface Props {
     activityId: string;
@@ -45,10 +46,9 @@ export default observer(function ActivityDetailedChat({ activityId }: Props) {
                                 <Comment.Metadata>
                                     <div>{comment.createdAt}</div>
                                 </Comment.Metadata>
-                                <Comment.Text>{comment.body}</Comment.Text>
-                                <Comment.Actions>
-                                    <Comment.Action>Reply</Comment.Action>
-                                </Comment.Actions>
+                                <Comment.Text style={{ whiteSpace: 'pre-wrap' }}>
+                                    {comment.body}
+                                </Comment.Text>
                             </Comment.Content>
                         </Comment>
                     ))}
@@ -57,20 +57,36 @@ export default observer(function ActivityDetailedChat({ activityId }: Props) {
                         onSubmit={(values, { resetForm }) =>
                             commentStore.addComment(values).then(() => resetForm())}
                         initialValues={{ body: '' }}
+                        validationSchema={Yup.object({
+                            body: Yup.string().required()
+                        })}
                     >
-                        {({ isSubmitting, isValid }) => (
+                        {({ isSubmitting, isValid, handleSubmit }) => (
                             <Form className='ui form'>
-                                <MyTextArea placeholder='AddComment' name='body' rows={2} />
-                                <Button
-                                    loading={isSubmitting}
-                                    disabled={isSubmitting || !isValid}
-                                    content='Add Reply'
-                                    labelPosition='left'
-                                    icon='edit'
-                                    primary
-                                    type='submit'
-                                    floated='right'
-                                />
+                                <Field name='body'>
+                                    {(props: FieldProps) => (
+                                        <div style={{ position: 'relative' }}>
+                                            <Loader active={isSubmitting} />
+                                            <textarea
+                                                placeholder='Enter you comment (Enter to submit, SHIFT + enter for new line)'
+                                                rows={2}
+                                                {...props.field}
+                                                onKeyPress={e => {
+                                                    if (e.key === 'Enter'
+                                                        && e.shiftKey) {
+                                                        return;
+                                                    }
+                                                    if (e.key === 'Enter' &&
+                                                        !e.shiftKey) {
+                                                        e.preventDefault();
+                                                        isValid && handleSubmit();
+                                                    }
+                                                }}
+                                            />
+                                        </div>
+                                    )}
+                                </Field>
+
                             </Form>
                         )}
                     </Formik>
