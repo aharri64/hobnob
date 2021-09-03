730551bc1fdded7d8fbaf14523f1ad97ee280a66
aharri64 <haveacompleteday@gmail.com>
2021-09-02 19:17:37 -0600

    adding th ui for followers

--- client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
@@ -10,6 +10,11 @@ interface Props {
 }
 
 export default observer(function ActivityListItemAttendee({ attendees }: Props) {
+    const styles = {
+        borderColor: 'orange',
+        borderWidth: 2
+    }
+
     return (
         <List horizontal>
             {attendees.map(attendee => (
@@ -26,6 +31,8 @@ export default observer(function ActivityListItemAttendee({ attendees }: Props) 
                                 size="mini"
                                 circular
                                 src={attendee.image || '/assets/user.png'}
+                                bordered
+                                style={attendee.following ? styles : null}
                             />
                         </List.Item>
                     }
