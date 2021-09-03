539ebebf8c55234ec1d4f72ea81146a2d43217a4
aharri64 <haveacompleteday@gmail.com>
2021-09-03 13:46:10 -0600

    using mobx reactions

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -2,6 +2,7 @@ import { observer } from 'mobx-react-lite'
 import React from 'react'
 import { Tab } from 'semantic-ui-react'
 import { Profile } from '../../app/models/profile'
+import { useStore } from '../../app/stores/store'
 import ProfileAbout from './ProfileAbout'
 import ProfileFollowings from './ProfileFollowings'
 import ProfilePhotos from './ProfilePhotos'
@@ -11,6 +12,8 @@ interface Props {
 }
 
 export default observer(function ProfileContent({ profile }: Props) {
+    const { profileStore } = useStore();
+
     const panes = [
         { menuItem: 'About', render: () => <ProfileAbout /> },
         { menuItem: 'Photos', render: () => <ProfilePhotos profile={profile} /> },
@@ -23,6 +26,7 @@ export default observer(function ProfileContent({ profile }: Props) {
             menu={{ fluid: true, vertical: true }}
             menuPosition='right'
             panes={panes}
+            onTabChange={(e, data) => profileStore.setActiveTab(data.activeIndex)}
         />
     )
 })
