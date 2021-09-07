169fffc51f1a076b0101cd91f75952fb404ccbcb
aharri64 <haveacompleteday@gmail.com>
2021-09-06 21:46:52 -0600

    adding a pagination header

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,7 +1,9 @@
+using API.Extentions;
 using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+
 
 
 namespace API.Controllers
@@ -18,6 +20,20 @@ namespace API.Controllers
             if (result == null) return NotFound();
             if (result.IsSuccess && result.Value != null)
                 return Ok(result.Value);
+            if (result.IsSuccess && result.Value == null)
+                return NotFound();
+            return BadRequest(result.Error);
+        }
+
+        protected ActionResult HandlePagedResult<T>(Result<PagedList<T>> result)
+        {
+            if (result == null) return NotFound();
+            if (result.IsSuccess && result.Value != null)
+            {
+                Response.AddPaginationHeader(result.Value.CurrentPage, result.Value.PageSize,
+                    result.Value.TotalCount, result.Value.TotalPages);
+                return Ok(result.Value);
+            }
             if (result.IsSuccess && result.Value == null)
                 return NotFound();
             return BadRequest(result.Error);
