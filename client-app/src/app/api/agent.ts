60ea61d1eb380c0069a86ac8c5692b13e42a6748
aharri64 <haveacompleteday@gmail.com>
2021-09-12 21:39:37 -0600

    adding events to user profile page

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,7 +4,7 @@ import { history } from '../..';
 
 import { Activity, ActivityFormValues } from '../models/activity';
 import { PaginatedResult } from '../models/pagination';
-import { Photo, Profile } from '../models/profile';
+import { Photo, Profile, UserActivity } from '../models/profile';
 import { User, UserFormValues } from '../models/user';
 import { store } from '../stores/store';
 
@@ -106,7 +106,9 @@ const Profiles = {
     updateProfile: (profile: Partial<Profile>) => requests.put(`profiles`, profile),
     updateFollowing: (username: string) => requests.post(`/follow/${username}`, {}),
     listFollowings: (username: string, predicate: string) =>
-        requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`)
+        requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`),
+    listActivities: (username: string, predicate: string) =>
+        requests.get<UserActivity[]>(`/profiles/${username}/activities?predicate=${predicate}`)
 }
 
 const agent = {
